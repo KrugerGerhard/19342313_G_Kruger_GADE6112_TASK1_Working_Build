c9bc5d4ab8a0d2c9765b20db0f092c4fc194e72a
Toxic_Saint (G) <71284936+KrugerGerhard@users.noreply.github.com>
2021-09-27 11:26:42 +0200

    Update EmptyTile.cs

--- EmptyTile.cs
+++ EmptyTile.cs
@@ -8,7 +8,7 @@ namespace _19342313_G_Kruger_GADE6112_TASK1
 {
     public class EmptyTile : Tile //Class for Empty Tiles for the Map
     {
-        public EmptyTile(int X, int Y) : base(X, Y)
+        public EmptyTile(int X, int Y) : base(X, Y) //X and Y values for Obstacles
         {
 
         }
